8e3808b9b77d37a6c7ae0690a352a5f975693ab2
Yudhishthirsh <130043970+Yudhishthirsh@users.noreply.github.com>
2023-05-18 16:56:13 +0530

    Update parameter.py

--- InstagramHastagDataScraper/parameter.py
+++ InstagramHastagDataScraper/parameter.py
@@ -7,3 +7,12 @@ Instagram_password = "Qazxsw@321"
 #Hashtag from which need to scrape data
 Instagram_Hashtag = "rentinnoida"
 
+# Define the Random file name
+length = 5
+# Generate a random string
+random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+# Get the current timestamp
+timestamp = time.strftime("%Y%m%d%H%M%S")
+fileName = timestamp + random_string + '.csv' #this the generated filename
+print("Your output file is: ",fileName )
+
